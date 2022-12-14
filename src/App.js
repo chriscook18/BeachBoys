eca9173a5f4d7d7c9c015a708a6a1b7429a1cbb2
chriscook18 <cook678@gmail.com>
2022-12-14 00:55:53 +0000

    1 framework

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+
+    render() {
+  
+      return (
+        <div className="container">
+        Yo    
+        </div>
+        )
+    }
+  }
+
+
+  
+export default App
